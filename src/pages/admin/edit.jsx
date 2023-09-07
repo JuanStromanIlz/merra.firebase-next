3eb819e925e0df13e01297ee6b181e500ccedc05
JuanStromanIlz <juanstroman@gmail.com>
2023-09-07 18:33:34 -0300

    X-X | import error

--- src/pages/admin/edit.jsx
+++ src/pages/admin/edit.jsx
@@ -1,38 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
-import { Heading, Stack } from "@chakra-ui/react";
-import createDoc from "../../actions/createDoc";
-import { PUBLICACIONES } from "../../services/foldersNames";
 import FolderForm from "../../components/FolderForm";
 import Title from "src/components/Title";
-import PageWrapper from "src/components/PageWrapper";
 import getDoc from "src/actions/getDoc";
 import updateDoc from "src/actions/updateDoc";
 import useFetch from "src/hooks/useFetch";
+import TextLayout from "src/components/TextLayout";
 
 export default function Edit() {
   const router = useRouter();
-  const { title, folder } = router.query;
-  const { data, loading } = useFetch(() => getDoc(title, folder));
+  const { title } = router.query;
+  const { data, loading } = useFetch(() => getDoc(title));
 
   const onUpdate = async (values) => {
     try {
-      const { category: folder, ...rest } = values;
-      if (folder !== PUBLICACIONES) {
-        await updateDoc({ ...rest }, folder);
-        router.push(`/${folder}/${values.title}`);
-      }
+      await updateDoc(values);
+      router.push(`/${values.title}`);
     } catch ({ message }) {
       console.error(message);
     }
   };
 
   return (
-    <PageWrapper pageTitle="Nueva publicacion">
+    <TextLayout>
       <Title as={"h1"} size={"4xl"} letterSpacing={"wider"} my={12}>
         {loading ? "loading" : "editar"}
       </Title>
       <FolderForm folder={data} onSubmit={onUpdate} />
-    </PageWrapper>
+    </TextLayout>
   );
 }
