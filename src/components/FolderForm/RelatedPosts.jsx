486c7b83ae36342496cee7230cd9dde343dd9a51
JuanStromanIlz <juanstroman@gmail.com>
2023-09-09 14:32:15 -0300

    X-X | RelatedPosts

--- src/components/FolderForm/RelatedPosts.jsx
+++ src/components/FolderForm/RelatedPosts.jsx
@@ -3,18 +3,24 @@ import React from 'react';
 import getRelatedDocs from 'src/actions/getRelatedDocs';
 import useFetch from 'src/hooks/useFetch';
 import Title from '../Title';
-import Item from '../Item';
+import Item from '../Post';
 
 const RelatedPosts = ({ doc }) => {
   const { data, loading } = useFetch(() => getRelatedDocs(doc));
 
-  if (!loading && !data) {
+  if (!loading && !data?.length) {
+    return null;
+  }
+
+  if (!data?.length) {
     return null;
   }
 
   return (
     <Flex direction={'column'}>
-      <Title my={6}>Relacionados</Title>
+      <Title my={6} borderBottomWidth={1} borderColor={'brand.500'}>
+        Relacionados
+      </Title>
       <SimpleGrid columns={{ base: 1, md: 3 }} gap={3}>
         {data?.map((i) => (
           <Item data={i} key={i?.id} />
