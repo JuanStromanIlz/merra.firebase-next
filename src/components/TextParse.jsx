e46d6011effae873b79a0182a29f70760bea0ef6
JuanStromanIlz <juanstroman@gmail.com>
2023-09-09 14:27:00 -0300

    X-X | TextParse

--- src/components/TextParse.jsx
+++ src/components/TextParse.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Box,
   Link,
@@ -6,51 +6,51 @@ import {
   UnorderedList,
   Text,
   ListItem,
-} from "@chakra-ui/react";
-import { ExternalLinkIcon } from "@chakra-ui/icons";
-import JsxParser from "react-jsx-parser";
-import Title from "./Title";
-import Quote from "./Quote";
+} from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+import JsxParser from 'react-jsx-parser';
+import Title from './Title';
+import Quote from './Quote';
 
-const TextParse = ({ children = "" }) => {
+const TextParse = ({ children = '' }) => {
   const heading1Open = "<Title as='h2' size='xl'>";
-  const heading1Close = "</Title>";
+  const heading1Close = '</Title>';
   const heading2Open = "<Title as='h3' size='lg'>";
-  const heading2Close = "</Title>";
+  const heading2Close = '</Title>';
   const heading3Open = "<Title as='h4' size='sm'>";
-  const heading3Close = "</Title>";
-  const linkOpen = "<Link color='pink.50' isExternal";
-  const linkClose = "</Link>";
-  const blockquoteOpen = "<Quote>";
-  const blockquoteClose = "</Quote>";
-  const olOpen = "<OrderedList spacing={3}>";
-  const olClose = "</OrderedList>";
-  const ulOpen = "<UnorderedList spacing={3}>";
-  const ulClose = "</UnorderedList>";
-  const listItemOpen = "<ListItem>";
-  const listItemClose = "</ListItem>";
-  const textOpen = "<Text>";
-  const textClose = "</Text>";
+  const heading3Close = '</Title>';
+  const linkOpen = "<Link color='pink.50'";
+  const linkClose = '</Link>';
+  const blockquoteOpen = '<Quote>';
+  const blockquoteClose = '</Quote>';
+  const olOpen = '<OrderedList spacing={3}>';
+  const olClose = '</OrderedList>';
+  const ulOpen = '<UnorderedList spacing={3}>';
+  const ulClose = '</UnorderedList>';
+  const listItemOpen = '<ListItem>';
+  const listItemClose = '</ListItem>';
+  const textOpen = '<Text>';
+  const textClose = '</Text>';
 
   const parsedChild = children
-    .replace("<h2>", heading1Open)
-    .replace("</h2>", heading1Close)
-    .replace("<h3>", heading2Open)
-    .replace("</h3>", heading2Close)
-    .replace("<h4>", heading3Open)
-    .replace("</h4>", heading3Close)
-    .replace("<a", linkOpen)
-    .replace("</a>", linkClose)
-    .replace("<blockquote>", blockquoteOpen)
-    .replace("</blockquote>", blockquoteClose)
-    .replace("<ul>", ulOpen)
-    .replace("</ul>", ulClose)
-    .replace("<ol>", olOpen)
-    .replace("</ol>", olClose)
-    .replace("<li>", listItemOpen)
-    .replace("</li>", listItemClose)
-    .replace("<p>", textOpen)
-    .replace("</p>", textClose);
+    .replace('<h2>', heading1Open)
+    .replace('</h2>', heading1Close)
+    .replace('<h3>', heading2Open)
+    .replace('</h3>', heading2Close)
+    .replace('<h4>', heading3Open)
+    .replace('</h4>', heading3Close)
+    .replace('<a', linkOpen)
+    .replace('</a>', linkClose)
+    .replace('<blockquote>', blockquoteOpen)
+    .replace('</blockquote>', blockquoteClose)
+    .replace('<ul>', ulOpen)
+    .replace('</ul>', ulClose)
+    .replace('<ol>', olOpen)
+    .replace('</ol>', olClose)
+    .replace('<li>', listItemOpen)
+    .replace('</li>', listItemClose)
+    .replace('<p>', textOpen)
+    .replace('</p>', textClose);
 
   return (
     <JsxParser
