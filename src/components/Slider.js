17a8d8845a82b3a5f7715af498d45abe3f442951
JuanStromanIlz <juanstroman@gmail.com>
2023-09-12 23:14:04 -0300

    X-X | Slider props

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,17 +1,27 @@
 import React, { useRef } from 'react';
 import useSliding from 'src/hooks/useSliding';
-import { Box, Button, Flex, useDimensions } from '@chakra-ui/react';
+import { Box, Button, Flex, useBreakpointValue } from '@chakra-ui/react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 
-const Slider = ({ items, Component = Box }) => {
-  const elementRef = useRef(null);
-  const { marginBox: { width } = {} } = useDimensions(elementRef) || {};
-  const { handlePrev, handleNext, slideProps, containerRef, hasNext, hasPrev } =
-    useSliding(width, items?.length);
+const Slider = ({ items, gap = 1, Component = Box }) => {
+  const itemsPerParent = useBreakpointValue({ base: 1, md: 2, lg: 3 });
+  const {
+    handlePrev,
+    handleNext,
+    slideProps,
+    itemLength,
+    containerRef,
+    hasNext,
+    hasPrev,
+  } = useSliding(itemsPerParent, items?.length);
+
+  if (!items?.length) {
+    return null;
+  }
 
   return (
     <Box overflow={'hidden'} position={'relative'}>
-      <Flex position={'relative'}>
+      <Flex position={'relative'} overflow={'hidden'}>
         <Flex
           ref={containerRef}
           sx={{ transition: 'transform 300ms ease 100ms' }}
@@ -24,9 +34,9 @@ const Slider = ({ items, Component = Box }) => {
                 position={'relative'}
                 flexGrow={0}
                 flexShrink={0}
-                flexBasis={'40%'}
+                flexBasis={itemLength}
                 key={index}
-                ref={elementRef}
+                pr={gap}
               >
                 <Component item={item} />
               </Box>
@@ -43,7 +53,7 @@ const Slider = ({ items, Component = Box }) => {
           bottom={0}
           left={0}
           height={'auto'}
-          w={width / 10}
+          w={itemLength / 10}
           colorScheme='transparent'
         >
           <ChevronLeftIcon w={10} h={10} />
@@ -59,7 +69,7 @@ const Slider = ({ items, Component = Box }) => {
           right={0}
           height={'auto'}
           colorScheme='transparent'
-          w={width / 10}
+          w={itemLength / 10}
         >
           <ChevronRightIcon w={10} h={10} />
         </Button>
--- src/hooks/useSliding.js
+++ src/hooks/useSliding.js
@@ -1,25 +1,28 @@
 import { useDimensions } from '@chakra-ui/react';
 import { useState, useRef, useEffect } from 'react';
 
-const useSliding = (elementWidth, countElements) => {
+const useSliding = (itemsPerParent, countElements) => {
   const containerRef = useRef(null);
   const { marginBox: { width } = {} } = useDimensions(containerRef) || {};
+  const [itemLength, setItemLength] = useState(0);
   const [distance, setDistance] = useState(0);
   const [totalInViewport, setTotalInViewport] = useState(0);
   const [viewed, setViewed] = useState(0);
 
   useEffect(() => {
-    setTotalInViewport(Math.floor(width / elementWidth));
-  }, [elementWidth, width]);
+    const item = itemsPerParent + 0.5;
+    setTotalInViewport(Math.floor(width / (width / item)));
+    setItemLength(width / item);
+  }, [itemLength, itemsPerParent, width]);
 
   const handlePrev = () => {
     setViewed(viewed - totalInViewport);
-    setDistance(distance + elementWidth * totalInViewport);
+    setDistance(distance + itemLength * itemsPerParent);
   };
 
   const handleNext = () => {
     setViewed(viewed + totalInViewport);
-    setDistance(distance - elementWidth * totalInViewport);
+    setDistance(distance - itemLength * itemsPerParent);
   };
 
   const slideProps = {
@@ -29,7 +32,15 @@ const useSliding = (elementWidth, countElements) => {
   const hasPrev = distance < 0;
   const hasNext = viewed + totalInViewport < countElements;
 
-  return { handlePrev, handleNext, slideProps, containerRef, hasPrev, hasNext };
+  return {
+    handlePrev,
+    handleNext,
+    slideProps,
+    containerRef,
+    itemLength,
+    hasPrev,
+    hasNext,
+  };
 };
 
 export default useSliding;
